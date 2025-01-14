8b9b2bace00ffc7f861c6fd81ae8ab9e3faadbda
daniamanal <110651547+daniamanal@users.noreply.github.com>
2025-01-14 17:45:50 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 # Let user input a word and a shift number.
 # Encode the message by shifting the letter by shift number. Write code to decode the message
+import art
+
+print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("type the shift number:\n"))
 
 
 def ceaser(original_text, shift_amount, encode_or_decode):
     output_text = ""
-    for i in original_text:
+    for letter in original_text:
 
-        if encode_or_decode == "decode":
-            shift_amount *= -1
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            if encode_or_decode == "decode":
+                shift_amount *= -1
 
-        shifted_position = alphabet.index(i) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
     print(f"here is the {encode_or_decode}d result: {output_text}")
 
 
-ceaser(text, shift, direction)
+should_continue = True
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("type the shift number:\n"))
+
+    ceaser(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
